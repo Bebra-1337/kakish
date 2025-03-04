1815ec4a880223e1432db96a9100b80fd0442a82
Bebra <romanovbebra@yandex.ru>
2025-03-05 00:52:25 +0300

    /

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hello World!" << endl;
     cout << "Ne Hello World" << endl;
     cout<<"anothe Hello World"<<endl;
+    cout<<"Ya sosal menya ebali"<<endl;
     return 0;
 }
